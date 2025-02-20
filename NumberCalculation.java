ca2c7cefc71a2f291374b3816f73331cc0980045
Repo User <repo@example.com>
2025-02-20 11:29:14 +0530

    Add NumberCalculation class with arithmetic operations
    Implemented the Calculator class with the following methods:
    - add: Addition
    - sub: Subtraction
    - div: Division
    - mult: Multiplication
    - sqrt: Square root

--- NumberCalculation.java
+++ NumberCalculation.java
@@ -1,3 +1,20 @@
 public int add(int a, int b){
 	return a+b ;
 }
+
+public int sub(int a, int b){
+        return a - b ;
+}
+
+public int div(int a, int b){
+        return a/b;
+}
+
+
+public int multiply(int a, int b){
+        return a*b;
+}
+
+public int sqrt(int a){
+        return Math.sqrt(a);
+}
